81efcef20a25758c65fb1bb4547121a18db580d0
ABHIJIT DAS <125692166+Abhijit2003das@users.noreply.github.com>
2025-10-28 14:56:02 +0530

    Rename Header title from 'SwiftCart' to 'QuickBasket'

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1>SwiftCart</h1>
+      <h1>QuickBasket</h1>
       <nav>
         <a href="#features" style={{ color: "white", marginRight: 20 }}>
           Features
